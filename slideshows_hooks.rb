13654ed75de2cc1e33f527a008a047a6a27f68da
Mosaic Interactive <valentino@mosaicwebsite.com>
2010-12-17 16:26:40 +0000

    Added a hook to display a gallery on the homepage by default

--- slideshows_hooks.rb
+++ slideshows_hooks.rb
@@ -1,7 +1,10 @@
 class SlideshowsHooks < Spree::ThemeSupport::HookListener
-
+  # Admin Hooks
   insert_after :admin_configurations_menu do
     %(<%= configurations_menu_item t('slideshow_slides'), admin_slides_path, t("manage_slideshow") %>)
   end
+
+  # Homepage hooks for default homepage slideshow
+  insert_before :homepage_products, 'shared/display_gallery'
 
 end
