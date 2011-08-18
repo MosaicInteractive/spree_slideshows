8fd23fc46556bdcc5bf435eab64d15a4cd651632
Valentino Stoll <valentino@mosaicwebsite.com>
2011-08-18 12:02:24 -0400

    Added caption and html slide customization

---
+++ db/migrate/20110818194413_add_caption_to_slideshows.rb
@@ -0,0 +1,9 @@
+class AddCaptionToSlideshows < ActiveRecord::Migration
+  def self.up
+    add_column :slides, :caption, :text
+  end
+
+  def self.down
+    remove_column :slides, :caption
+  end
+end
